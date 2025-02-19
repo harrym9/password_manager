1f98ce1ad09fbf9992c870fd3197bdd64bd69780
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-19 20:52:47 +0300

    changed from pack to grid format. Added labels, entry's and buttons

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -10,9 +9,32 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-logo =PhotoImage(file="logo.png")
-canvas = Canvas(width=300, height=300)
-canvas.create_image(150, 150, image=logo)
-canvas.pack()
+# Logo
+logo = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(102, 102, image=logo)
+canvas.grid(column=1, row=0)
+
+# Labels
+service_label = Label(text="Service/Website:")
+service_label.grid(column=0, row=1)
+email_label = Label(text="Email/Username")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+# Entry's
+service_entry = Entry(width=51)
+service_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=51)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=32)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_button = Button(text="Generate password", width=14)
+generate_button.grid(row=3, column=2)
+add_button = Button(text="Add password", width=43)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
